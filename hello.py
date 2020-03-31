d5947364a38584688a6879ca095eee93f051d80e
zsx0728 <zsx0728@163.com>
2020-04-01 06:17:28 +0800

    第一版本提交

---
+++ hello.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+from flask import Flask
+
+app=Flask(__name__)
+app.config['DEBUG']=True
+
+app.config.update(
+        DEBUG=True
+        )
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=9000)
